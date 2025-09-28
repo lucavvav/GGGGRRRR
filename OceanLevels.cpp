84db5428ecf364ba3a2695c801fdf564eef6860a
luca <luca01vv@gmail.com>
2025-09-28 10:59:05 -0400

    Changed name of Oceanlevels file

--- OceanLevels.cpp
+++ OceanLevels.cpp
@@ -11,9 +11,13 @@ int main()
     int yearsPassed2 = 7;
     int yearsPassed3 = 10;
     double inchesPerYear = 1.5;
+
+
     double oceanLevelRaised1 = yearsPassed1 * inchesPerYear;
     double oceanLevelRaised2 = yearsPassed2 * inchesPerYear;
     double oceanLevelRaised3 = yearsPassed3 * inchesPerYear;
+
+
     std::cout << "Ocean level after " << yearsPassed1 << " years is higher than the current level by " << oceanLevelRaised1 << " millimeters" << std::endl;
     std::cout << "Ocean level after " << yearsPassed2 << " years is higher than the current level by " << oceanLevelRaised2 << " millimeters" << std::endl;
     std::cout << "Ocean level after " << yearsPassed3 << " years is higher than the current level by " << oceanLevelRaised3 << " millimeters" << std::endl;
